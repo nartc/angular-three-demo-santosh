eacab7d0145fa8dcba9623791af2c6c349503a3f
Chau Tran <nartc7789@gmail.com>
2023-03-07 20:26:42 -0600

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { extend, injectBeforeRender, NgtArgs, NgtCanvas, NgtPush } from 'angular-three';
+import { NgtsOrbitControls } from 'angular-three-soba/controls';
+import { injectNgtsGLTFLoader } from 'angular-three-soba/loaders';
+import { NgtsEnvironment, NgtsFloat } from 'angular-three-soba/staging';
+import { map } from 'rxjs';
+import * as THREE from 'three';
+import { Color, Fog, Mesh, PlaneGeometry, PointLight, ShadowMaterial, Vector2 } from 'three';
+
+extend({ Mesh, Fog, PlaneGeometry, ShadowMaterial, PointLight, Vector2, Color });
+
+@Component({
+    standalone: true,
+    template: `
+        <ngt-fog *args="['#cccccc', 10, 15]" attach="fog" />
+        <ngt-color *args="['#95CFFF']" attach="background" />
+
+        <ngt-point-light [position]="[5, 15, 15]" [castShadow]="true" [intensity]="0.5">
+            <ngt-vector2 *args="[1024, 1024]" attach="shadow.mapSize" />
+        </ngt-point-light>
+
+        <ngt-mesh [receiveShadow]="true" [rotation]="[-Math.PI / 2, 0, 0]">
+            <ngt-plane-geometry *args="[100, 100]" />
+            <ngt-shadow-material [transparent]="true" />
+        </ngt-mesh>
+
+        <ngt-primitive
+            *args="model$ | ngtPush"
+            [scale]="0.01"
+            [position]="[0, 0.41, 0]"
+            [rotation]="[0, -Math.PI / 2, 0]"
+        />
+
+        <ngts-environment preset="sunset" />
+        <ngts-orbit-controls [autoRotate]="true" [autoRotateSpeed]="0.5" />
+    `,
+    imports: [NgtArgs, NgtPush, NgtsOrbitControls, NgtsEnvironment, NgtsFloat],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class Scene {
+    readonly Math = Math;
+    private mixer?: THREE.AnimationMixer;
+
+    readonly model$ = injectNgtsGLTFLoader('assets/scene.gltf').pipe(
+        map((gltf) => {
+            const scene = gltf.scene;
+            this.mixer = new THREE.AnimationMixer(scene);
+            this.mixer.clipAction(gltf.animations[0]).play();
+
+            gltf.scene.traverse((child) => {
+                if (child.isObject3D) {
+                    child.castShadow = true;
+                }
+            });
+
+            return [scene];
+        })
+    );
+
+    constructor() {
+        injectBeforeRender(({ delta }) => {
+            if (this.mixer) {
+                this.mixer.update(delta);
+            }
+        });
+    }
+}
 
 @Component({
     selector: 'app-root',
     standalone: true,
     template: `
-        <!--The content below is only a placeholder and can be replaced.-->
-        <div style="text-align:center" class="content">
-            <h1>Welcome to {{ title }}!</h1>
-            <span style="display: block">{{ title }} app is running!</span>
-            <img
-                width="300"
-                alt="Angular Logo"
-                src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg=="
-            />
-        </div>
-        <h2>Here are some links to help you start:</h2>
-        <ul>
-            <li>
-                <h2>
-                    <a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a>
-                </h2>
-            </li>
-            <li>
-                <h2>
-                    <a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a>
-                </h2>
-            </li>
-            <li>
-                <h2>
-                    <a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a>
-                </h2>
-            </li>
-        </ul>
+        <ngt-canvas [sceneGraph]="scene" [camera]="{ position: [-5, 5, 5] }" [shadows]="true" />
     `,
+    imports: [NgtCanvas, NgtArgs],
     styles: [],
 })
 export class AppComponent {
-    title = 'ng-three-demo';
+    readonly scene = Scene;
 }
